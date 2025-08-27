89dc7f05e08fb354902a4baab3a7956331198a3f
Md Jobarul Islam <jobarulislam1203@gmail.com>
2025-08-27 23:32:05 +0600

    Update print_ascii.asm

--- print_ascii.asm
+++ print_ascii.asm
@@ -6,45 +6,17 @@ main:
     mov ax, @data
     mov ds, ax
 
-    mov al, 80h    
-    mov cx, 128     
+    mov cx, 256     
+    mov al, 0       
 
-print_ext_ascii:
+print_loop:
     mov ah, 02h
     mov dl, al
-    int 21h
-
- 
-    mov dl, ' '
-    int 21h
-
+    int 21h         
     inc al
-
-   
-    push ax
-    mov ah, 0
-    mov al, cl
-    mov ah, 0
-    mov dx, cx
-    mov ax, 128
-    sub ax, cx
-    mov bl, 10
-    div bl
-    cmp ah, 0
-    jne skip_newline
-
-    
-    mov dl, 10
-    mov ah, 02h
-    int 21h
-    mov dl, 13
-    int 21h
-
-skip_newline:
-    pop ax
-    loop print_ext_ascii
+    loop print_loop
 
     mov ah, 4ch
     int 21h
-   main endp
+    main endp
 end main
